f68eb3dda97f8f35a5318cb5c9d593363bc475ad
Valentin Konov <ValentineK@users.noreply.github.com>
2026-02-20 15:03:12 +0100

    fix treesitter config: use main field instead of config fn

--- deploy/.config/nvim/init.lua
+++ deploy/.config/nvim/init.lua
@@ -51,17 +51,16 @@ require("lazy").setup({
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "lua", "python", "javascript", "typescript", "tsx",
-                    "bash", "ruby", "terraform", "hcl", "dockerfile", "yaml", "json",
-                    "rust",
-                },
-                highlight = { enable = true },
-                indent   = { enable = true },
-            })
-        end,
+        main  = "nvim-treesitter.configs",
+        opts  = {
+            ensure_installed = {
+                "lua", "python", "javascript", "typescript", "tsx",
+                "bash", "ruby", "terraform", "hcl", "dockerfile", "yaml", "json",
+                "rust",
+            },
+            highlight = { enable = true },
+            indent    = { enable = true },
+        },
     },
 
     -- LSP servers + completion
